2592fed5d57f44161722a527ec229d35e9c2839d
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 21:28:16 +0300

    operator: debounceTime

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { fromEvent } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { debounceTime, map } from 'rxjs/operators'
 
 const url = 'https://api.github.com/search/users?q=';
 
@@ -7,6 +7,7 @@ const search = document.getElementById('search')
 
 const stream$ = fromEvent(search, 'input').pipe(
     map(v=>v.target.value),
+    debounceTime(500)
 )
 
 stream$.subscribe(v=>console.log(v));
