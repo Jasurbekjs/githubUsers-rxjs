28caabf18c00e8548f05bf5b52323e3d9ae4c762
Jasurbek <jasmirzaev@gmail.com>
2023-11-04 21:36:31 +0300

    ajax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fromEvent } from 'rxjs'
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators'
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
 
 const url = 'https://api.github.com/search/users?q=';
 
@@ -8,7 +9,11 @@ const search = document.getElementById('search')
 const stream$ = fromEvent(search, 'input').pipe(
     map(v=>v.target.value),
     debounceTime(500),
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    switchMap(
+        v => ajax.getJSON(url + v)
+    ),
+    map(response => response.items)
 )
 
 stream$.subscribe(v=>console.log(v));
